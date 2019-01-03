a08191a96ba7da2b680445b18b7675596d87651e
zhangningl <zhangningle@chingsoft.com>
2019-01-03 16:27:03 +0800

    works 完成

--- js/index.js
+++ js/index.js
@@ -1,16 +1,3 @@
-
-var banneri= document.querySelector('.banner-li').children;
-banneri[0].onmouseover= function(){
-    bannerImg.src="images/chatu2.png";
-    banneri[0].style.backgroundColor = "#ff9985";
-    banneri[1].style.backgroundColor = "#848484";
-}
-banneri[1].onmouseover= function(){
-    bannerImg.src="images/chatu1.png";
-    banneri[0].style.backgroundColor = "#848484";
-    banneri[1].style.backgroundColor = "#ff9985";
-}
-
 weixinShow.onmouseover=function(){
     weixinImg.style.display="block"
 }
@@ -18,3 +5,23 @@ wechat.onmouseout=function(){
     weixinImg.style.display="none"
 }
 
+var bannerMain1= document.querySelector('.banner-main1');
+var bannerMain2= document.querySelector('.banner-main2');
+if(bannerMain1){
+    var banneri= document.querySelector('.banner-li').children;
+    banneri[0].onmouseover= function(){
+        bannerMain1.style.display= 'block';
+        bannerMain2.style.display= 'none';
+        banneri[0].style.backgroundColor = "#ff9985";
+        banneri[1].style.backgroundColor = "#848484";
+    }
+    banneri[1].onmouseover= function(){
+        bannerMain1.style.display= 'none';
+        bannerMain2.style.display= 'block';
+        banneri[0].style.backgroundColor = "#848484";
+        banneri[1].style.backgroundColor = "#ff9985";
+    }
+    
+}else{
+    console.log("undefined");
+}
